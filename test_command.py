5ae9c7c983210d08dce4aecd10ecce6953bbe61d
knownothing1028 <yczhang1028@gmail.com>
2019-09-16 16:25:15 -0400

    mmr, xp, gold, kills functions have been done

--- test_command.py
+++ test_command.py
@@ -56,7 +56,10 @@ def get_player_info_match_id(player_id, match_id):
     :return:
     '''
     players_info = get_players_info_match_id(match_id)
+
     for player_info in players_info:
+        if(player_info is None):
+            continue
         if player_info['account_id'] == player_id:
             return player_info
         else:
@@ -89,21 +92,26 @@ def get_xp_info_player_match(player_id,match_id, prt=False):
     :return:
     """
     info_player = get_player_info_match_id(player_id,match_id)
+
     xp_info =info_player['xp_reasons']
+    if(xp_info is None):
+        print("None")
+        return None
     if prt == True:
-        for i in info_player.keys():
+        print("Match ID is",match_id, "Total Xp is:",info_player['total_xp'])
+        for i in xp_info.keys():
             if i == '0':
-                print('Unspecified:', xp_info[i])
+                print('Unspecified:', xp_info[i],end="    \t")
             if i == '1':
-                print('HeroKill:', xp_info[i])
+                print('HeroKill:', xp_info[i],end="    \t")
             if i == '2':
-                print('CreepKill:', xp_info[i])
+                print('CreepKill:', xp_info[i],end="    \t")
             if i == '3':
-                print('RoshanKill:', xp_info[i])
+                print('RoshanKill:', xp_info[i],end="    \t")
+        print()
     return xp_info
 
 def get_totoal_xp(player_id,match_id,prt=False):
-    player = Player(player_id)
     info_player = get_player_info_match_id(player_id,match_id)
     if prt == True:
         print("Total XP:",info_player['total_xp'])
@@ -119,102 +127,206 @@ def get_xp_info_player_recent_matches(player_id,prt=False):
     """
     xp_info_recent_matches =[]
     matches_recent_id = get_recent_matches_id(player_id)
-    for m_id in matches_recent_id:
-        xp_info_recent_matches.append(get_xp_info_player_match(player_id,m_id,prt))
-    return  xp_info_recent_matches
 
+    for m_id in matches_recent_id:
+        xp_info = get_xp_info_player_match(player_id,m_id,prt)
+        if(xp_info is None):
+            continue
+        xp_info_recent_matches.append(xp_info)
+
+    return xp_info_recent_matches
+
+
+def get_gold_info_player_match(player_id,match_id, prt=False):
+    """
+    get the gold information of one player in one match
+    :param player_id:
+    :param match_id:
+    :return:
+    """
+    info_player = get_player_info_match_id(player_id,match_id)
+
+    gold_info =info_player['gold_reasons']
+    if(gold_info is None):
+        print("None")
+        return None
+    if prt == True:
+        print("Match ID is",match_id, "Total GOLD is:",info_player['total_gold'])
+        count =0
+        l = len(gold_info)
+        for i in gold_info.keys():
+            if i == '0':
+                print('Unspecified:', gold_info[i],end="    \t")
+            if i == '1':
+                print('DeathLost:', gold_info[i], end="    \t")
+            if i == '2':
+                print('Buyback:', gold_info[i], end="    \t")
+            if i == '3':
+                print('PurchaseConsumable:', gold_info[i], end="    \t")
+            if i == '4':
+                print('PurchaseItem:', gold_info[i], end="    \t")
+            if i == '5':
+                print('AbandonedRedistribute:', gold_info[i], end="    \t")
+            if i == '6':
+                print('SellItem:', gold_info[i], end="    \t")
+            if i == '7':
+                print('AbilityCost:', gold_info[i], end="    \t")
+            if i == '8':
+                print('CheatCommand:', gold_info[i], end="    \t")
+            if i == '9':
+                print('SelectionPenalty:', gold_info[i], end="    \t")
+            if i == '10':
+                print('GameTick:', gold_info[i], end="    \t")
+            if i == '11':
+                print('Building:', gold_info[i], end="    \t")
+            if i == '12':
+                print('Herokill:', gold_info[i], end="    \t")
+            if i == '13':
+                print('CreepKill:', gold_info[i], end="    \t")
+            if i == '14':
+                print('RoshanKill:', gold_info[i], end="    \t")
+            if i == '15':
+                print('CourierKill:', gold_info[i], end="    \t")
+            if i == '16':
+                print('SharedGold:', gold_info[i], end="    \t")
+            count += 1
+            if (count % 4==0 and count != l):
+                print()
+        print()
+    return gold_info
 
 def get_totoal_gold(player_id,match_id,prt=False):
-    player = Player(player_id)
     info_player = get_player_info_match_id(player_id,match_id)
-    if prt==True:
-        print("Total Gold:",info_player['total_gold'])
+    if prt == True:
+        print("Total GOLD:",info_player['total_gold'])
     return info_player['total_gold']
 
-# '''Match_id = 5018080036
-# Player_id = 143593296
-#
-# matches_recent_id = find_recent_match(Player_id)
-# print(matches_recent_id)
-# info_recent_matches_player = get_info_recent_matches_player(Player_id)
-# # print(info_recent_matches_player)
-#
-# count =0
-# for info in info_recent_matches_player:
-# 	# print('info',info)
-# 	# print('info_gold_reasons',info['gold_reasons'])
-# 	# print('type of info gold',type(info['gold_reasons']))
-# 	if info['gold_reasons'] is None:
-# 		continue
-# 	else:
-# 		keys = info['gold_reasons'].keys()
-# 		if '0' in keys:
-# 			print(info['gold_reasons']['0'])
-# 			count +=info['gold_reasons']['0']
-# 		else:
-# 			print('No 9')
-#
-#
-#
-# # m = Match(5018080036)
-# # for i in m.players:
-# 	# print(i)
-# 	# print('\n')
-# 	# print(i['account_id'])
-# 	# query_mmr(i['account_id'])
-# 	# print('\n')
-# # p = m.players
-# # p0 = p[0]
-# # print(p0['account_id'])
-# # print(p0.keys())
-# # print(p0['gold_reasons'])
-# # match = Match(5016567818)
-#
-#
-# # id = 321174338
-# # p = Player(143593296)
-# # print(p.recent_matches[0])
-# # print(p.rankings)
 
+def get_gold_info_player_recent_matches(player_id,prt=False):
+    """
+    get the gold information of ONE player for recent matches
+
+    :param player_id:
+    :return:list of xp information for recent matches
+    """
+    gold_info_recent_matches =[]
+    matches_recent_id = get_recent_matches_id(player_id)
+
+    for m_id in matches_recent_id:
+        gold_info = get_gold_info_player_match(player_id,m_id,prt)
+        if(gold_info is None):
+            continue
+        gold_info_recent_matches.append(gold_info)
+
+    return gold_info_recent_matches
+
+def get_kills_info_player_match(player_id,match_id,prt=False):
+    """
+    get ONE player in ONE match's kills information
+    :param player_id:
+    :param match_id:
+    :param prt:
+    :return: list of kill information
+    """
+    info = get_player_info_match_id(player_id,match_id)
+    info_kills = {}
+    if('hero_kills' not in info.keys()):
+        if prt is True:
+            print("None")
+        return None
+    info_kills.update({'hero_kills': info['hero_kills'] } )
+    info_kills.update({'lane_kills':info['lane_kills']})
+    info_kills.update({'neutral_kills':info['neutral_kills']})
+    info_kills.update({'ancient_kills':info['ancient_kills']})
+    info_kills.update({'tower_kills':info['tower_kills']})
+    info_kills.update({'roshan_kills':info['roshan_kills']})
+    info_kills.update({'observer_kills':info['observer_kills']})
+    info_kills.update({'courier_kills':info['courier_kills']})
+    info_kills.update({'observer_kills':info['observer_kills']})
+    info_kills.update({'sentry_kills':info['sentry_kills']})
+    info_kills.update({'necronomicon_kills':info['necronomicon_kills']})
+    if(prt is True):
+        print(info_kills)
+    return info_kills
+
+def get_kills_info_player_recent_matches(player_id,prt = False):
+    """
+    get kills information for recent matches
+    :param player_id:
+    :param prt:
+    :return: list of kills information dictionary
+    """
+    info_kills =[]
+    match_id_list = get_recent_matches_id(player_id)
+    for m_id in match_id_list:
+        info_kills.append(get_kills_info_player_match(player_id,m_id,prt))
+
+    return  info_kills
+
+# '''Match_id = 5025106005
+# Player_id = 143593296
+
+# -----------------------------------------------------------
 if __name__ == '__main__':
     cmd = sys.argv[1]
     if cmd.lower() == 'mmr':
         player_id = int(sys.argv[2])
         query_mmr(player_id)
 
-    if cmd.lower() == 'farm':
-        player_id = int(sys.argv[2])
-        matches_recent_id = get_recent_matches_id(player_id)
-        info_player_recent_matches = get_player_info_recent_matches(player_id)
-        sum_of_ten_matches = 0
-        count = 0
-        for info in info_player_recent_matches:
-            total_prop = 0
-            if info['gold_reasons'] is None:
-                continue
-            else:
-                keys = info['gold_reasons'].keys()
-                for key in keys:
-                    total_prop += info['gold_reasons'][key]
-                print('Match ID: ' + str(info['match_id']))
-                print('total_property: ' + str(total_prop))
-                print(' ')
-                sum_of_ten_matches += total_prop
-                count += 1
-        ave_of_ten_matches = sum_of_ten_matches / count
-        print('Average property of past', count, 'matches: ' + str(int(ave_of_ten_matches)))
-
+    # get information about experience
     if cmd.lower() == 'xp':
-        if len(sys.argv)==3:
-            player_id = int(sys.argv[2])
-            prt = False
-            get_xp_info_player_recent_matches(player_id,prt)
-        if len(sys.argv)==4:
-            player_id = int(sys.argv[2])
-            if(sys.argv[3] and sys.argv[3]=='True'):
+        player_id = int(sys.argv[2])
+        prt = False
+        l = len(sys.argv)
+        if (l == 3):
+            get_xp_info_player_recent_matches(player_id)
+        if(l==4):
+            if (sys.argv[3].lower() == "true"or sys.argv[3].lower() == "t"):
                 prt = True
                 get_xp_info_player_recent_matches(player_id,prt)
             else:
                 match_id = int(sys.argv[3])
-                get_xp_info_player_match(player_id,match_id)
+                get_xp_info_player_match(player_id,match_id,prt)
+        if(l==5):
+            prt = True
+            match_id = int(sys.argv[3])
+            get_xp_info_player_match(player_id, match_id, prt)
+
+    # for query information about gold
+    if cmd.lower() == 'gold':
+        player_id = int(sys.argv[2])
+        prt = False
+        l = len(sys.argv)
+        if (l == 3):
+            get_gold_info_player_recent_matches(player_id)
+        if(l==4):
+            if (sys.argv[3].lower() == "true"or sys.argv[3].lower() == "t"):
+                prt = True
+                get_gold_info_player_recent_matches(player_id,prt)
+            else:
+                match_id = int(sys.argv[3])
+                get_gold_info_player_match(player_id,match_id,prt)
+        if(l==5):
+            prt = True
+            match_id = int(sys.argv[3])
+            get_gold_info_player_match(player_id, match_id, prt)
+
+    # get kills information
+    if cmd.lower() == 'kills' or cmd.lower() == 'kill':
+        player_id = int(sys.argv[2])
+        prt = False
+        l = len(sys.argv)
+        if (l == 3):
+            get_kills_info_player_recent_matches(player_id)
+        if(l==4):
+            if (sys.argv[3].lower() == "true"or sys.argv[3].lower() == "t"):
+                prt = True
+                get_kills_info_player_recent_matches(player_id,prt)
+            else:
+                match_id = int(sys.argv[3])
+                get_kills_info_player_match(player_id,match_id,prt)
+        if(l==5):
+            prt = True
+            match_id = int(sys.argv[3])
+            get_kills_info_player_match(player_id, match_id, prt)
 
